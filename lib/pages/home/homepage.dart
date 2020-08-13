de351a8081b55c7a47e08835646624a90d75c546
Charlie Pauvré <chpauvre@gmail.com>
2020-08-13 17:32:01 +0200

    Admin de la homepage

--- lib/pages/home/homepage.dart
+++ lib/pages/home/homepage.dart
@@ -6,25 +6,12 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:barbart/components/pagelistheader.dart';
 
-class HomePage extends StatelessWidget implements AbstractPageComponent{
+class HomePage extends StatefulWidget implements AbstractPageComponent{
+
+
   HomePage({Key key}) : super(key: key);
-
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AbstractPageComponent.backgroundColor,
-
-      key: _scaffoldKey,
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index){
-          return HomeListViewItem(liked: false, commented: false,)
-        ;}
-      )
-    );
-  }
 
   @override
   String get name => "Accueil";
@@ -35,14 +22,66 @@ class HomePage extends StatelessWidget implements AbstractPageComponent{
   @override
   Image get logo => Image(image: AssetImage("assets/logo_clipped.png"));
 
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+
+}
+
+class _HomePageState extends State<HomePage>{
+
+  bool admin;
+
+  @override
+  void initState() {
+    admin=false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: AbstractPageComponent.backgroundColor,
+        body: Stack(
+          children: <Widget>[
+            ListView.builder(
+                padding: EdgeInsets.only(bottom: 250),
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index){
+                  return HomeListViewItem(liked: false, commented: false, id: index)
+                  ;}
+            ),
+            /*Positioned(
+              bottom: 280,
+              right: 30,
+              child: Container(
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green,
+                ),
+                child: IconButton(
+                  color: admin ? Colors.blue : Colors.grey[400],
+                  icon: Icon(Icons.settings),
+                  onPressed: (){setState(() {
+                    admin = !admin;
+                  });},
+                ),
+              ),
+            ),*/
+          ],
+        )
+    );
+  }
+
 }
 
 class HomeListViewItem extends StatefulWidget{
 
   final bool liked;
   final bool commented;
+  final int id;
 
-  const HomeListViewItem({Key key, this.liked = false, this.commented = false}) : super(key: key);
+  const HomeListViewItem({Key key, this.liked = false, this.commented = false, this.id}) : super(key: key);
 
 
   @override
@@ -54,6 +93,12 @@ class _HomeListViewItemState extends State<HomeListViewItem> with SingleTickerPr
 
   bool _liked;
   bool _commented;
+  bool _admin;
+  bool _editingMainText, _editingTags;
+  String _tags, _mainText;
+
+  TextEditingController _tagController;
+  TextEditingController _mainTextController;
 
   AnimationController _controller;
   Animation<Color> _colorAnimationLikes;
@@ -62,10 +107,27 @@ class _HomeListViewItemState extends State<HomeListViewItem> with SingleTickerPr
   double _minSizeIcon = 20;
   double _maxSizeIcon = 30;
 
+  static const String Delete = "Delete";
+  static const String Tags = "Change tags";
+  static const String MainText = "Change text";
+
+  List<String> adminActions = [
+    Delete, Tags, MainText
+  ];
+
   @override
   void initState() {
     _liked = widget.liked;
     _commented = widget.commented;
+    _admin = true;
+
+    _tags = '[WEI][SOIREE FOLLE]';
+    _mainText = lorem;
+    _editingTags = false;
+    _editingMainText = false;
+
+    _tagController = TextEditingController(text: _tags);
+    _mainTextController = TextEditingController(text: _mainText);
 
     _controller = AnimationController(
       duration: Duration(milliseconds: 200),
@@ -97,10 +159,17 @@ class _HomeListViewItemState extends State<HomeListViewItem> with SingleTickerPr
     super.initState();
   }
 
-
+  @override
+  void dispose() {
+    _controller.dispose();
+    _tagController.dispose();
+    _mainTextController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    //print(_admin);
     return AnimatedBuilder(
       animation: _controller,
       builder: (BuildContext context, _){
@@ -128,18 +197,121 @@ class _HomeListViewItemState extends State<HomeListViewItem> with SingleTickerPr
                       )
                     ],
                   ),
-                  IconButton(
+                  ! _admin ? Container() : PopupMenuButton(
                     icon: Icon(Icons.more_horiz), //TODO: Implements onPressed or delete the widget
+                    itemBuilder: (BuildContext context){
+                      return adminActions.map((String actionStr) {
+                        return PopupMenuItem<String>(
+                          value: actionStr,
+                          child: Text(actionStr)
+                        );
+                      }).toList();
+                    },
+                    onSelected: (str) async{
+                      if (str == Delete){
+                        print("delete item number ${widget.id}"); // TODO: Implements function to delete this post
+                      }
+                      else if (str == Tags) {
+                        print("tags item number ${widget.id}"); // TODO: Implements function to change post in server
+                        setState(() {
+                          _editingTags = true;
+                        });
+                      }
+                      else if (str == MainText){
+                        print("text item number ${widget.id}");
+                        setState(() {
+                          _editingMainText = true;
+                        });
+                      }
+                    },
                   )
                 ],
               ),
               Container(
                 padding: EdgeInsets.all(10),
-                child: Text('[WEI][SOIREE FOLLE]', style: TextStyle(fontSize: 17),),// tags
+                child: !_editingTags ? Text(_tags, style: TextStyle(fontSize: 17)) :
+                Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: _tagController,
+                      minLines: 1,
+                      maxLines: 100,
+                      textAlign: TextAlign.justify,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.cancel, color: Colors.red),
+                          onPressed: (){
+                            setState(() {
+                              _tagController.clear();
+                              _tagController = TextEditingController(text: _tags);
+                              _editingTags = false;
+                            });
+                          },
+                          padding: EdgeInsets.all(5),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.check_circle, color: Colors.green),
+                          onPressed: (){
+                            setState(() {
+                              // TODO: change text in server
+                              _tags = _tagController.value.text;
+                              _tagController.clear();
+                              _tagController = TextEditingController(text: _tags);
+                              _editingTags = false;
+                            });
+                          },
+                          padding: EdgeInsets.all(5),
+                        )
+                      ],
+                    )
+                  ],
+                ),// TEXT),// tags
               ),
               Container(
                 padding: EdgeInsets.all(10),
-                child: Text(lorem),// TEXT
+                child: !_editingMainText ? Text(_mainText) :
+                Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: _mainTextController,
+                      minLines: 1,
+                      maxLines: 100,
+                      textAlign: TextAlign.justify,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.cancel, color: Colors.red),
+                          onPressed: (){
+                            setState(() {
+                              _mainTextController.clear();
+                              _mainTextController = TextEditingController(text: _mainText);
+                              _editingMainText = false;
+                            });
+                          },
+                          padding: EdgeInsets.all(5),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.check_circle, color: Colors.green),
+                          onPressed: (){
+                            setState(() {
+                              // TODO: change text in server
+                              _mainText = _mainTextController.value.text;
+                              _mainTextController.clear();
+                              _mainTextController = TextEditingController(text: _mainText);
+                              _editingMainText = false;
+                            });
+                          },
+                          padding: EdgeInsets.all(5),
+                        )
+                      ],
+                    )
+                  ],
+                ),// TEXT
               ),
               Container(
                 margin: EdgeInsets.only(left: 20, right: 20),
@@ -207,28 +379,56 @@ class _HomeListViewItemState extends State<HomeListViewItem> with SingleTickerPr
                 child: ListView.builder(
                   padding: EdgeInsets.only(top: 10),
                   itemCount: 10,
-                  itemBuilder: (BuildContext context, _){
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          CircleAvatar(
-                            backgroundImage: AssetImage('assets/logo.png'),
-                          ),
-                          SizedBox(width: 10,),
-                          Container(
-                            width: MediaQuery.of(context).size.width - 120,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text('Name Firstname', style: TextStyle(color: Colors.black,fontSize: 15, fontWeight: FontWeight.bold),),
-                                RichText(textAlign: TextAlign.justify,text:TextSpan(text: lorem.substring(0, 100), style: TextStyle(fontSize: 12, color: Colors.black, ),)),
-                                Text('20 min ago', style: TextStyle(color: Colors.grey[400]),)
-                              ],
+                  itemBuilder: (BuildContext context, int index){
+                    return GestureDetector(
+                      onLongPress: () async{
+                        if (_admin){
+                          await showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                            title: Text('Delete this item ?'),
+                            actions: <Widget>[
+                              FlatButton(
+                                child: Text("No"),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              FlatButton(
+                                child: Text("Yes"),
+                                onPressed: () {
+                                  setState(() {
+                                    print("The item $index has been deleted"); // TODO : Change value in server
+                                  });
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ],
+                          ));
+                        }
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            CircleAvatar(
+                              backgroundImage: AssetImage('assets/logo.png'),
                             ),
-                          )
-                        ],
+                            SizedBox(width: 10,),
+                            Container(
+                              width: MediaQuery.of(context).size.width - 120,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text('Name Firstname', style: TextStyle(color: Colors.black,fontSize: 15, fontWeight: FontWeight.bold),),
+                                  RichText(textAlign: TextAlign.justify,text:TextSpan(text: lorem.substring(0, 100), style: TextStyle(fontSize: 12, color: Colors.black, ),)),
+                                  Text('20 min ago', style: TextStyle(color: Colors.grey[400]),)
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     )
                   ;}
